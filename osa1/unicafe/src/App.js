5d9f55286138894f76cb9338bd6bd072a952c999
Susanna Haataja <susanha@student.uef.fi>
2021-10-31 11:25:00 +0200

    commit

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -6,7 +6,17 @@ const App = () => {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  return <div>code here</div>;
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <div>{good}</div>
+      <button onClick={() => setGood(good + 1)}>good</button>
+      <div>{neutral}</div>
+      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+      <div>{bad}</div>
+      <button onClick={() => setBad(bad + 1)}>bad</button>
+    </div>
+  );
 };
 
 export default App;
